8d3799a948020a142d313e6456b8a9febba98092
scloudq <92770707+scloudq@users.noreply.github.com>
2022-02-18 15:09:10 +0800

    Update my_ctf.py
    Final version of Myctf

--- Modifications/Demo/my_ctf.py
+++ Modifications/Demo/my_ctf.py
@@ -1,6 +1,10 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+# +
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 """Model a toy Capture the flag exercise
 
 See Jupyter notebook toyctf-simulation.ipynb for an example of
@@ -9,6 +13,9 @@ game played on this simulation.
 from cyberbattle.simulation import model as m
 from cyberbattle.simulation.model import NodeID, NodeInfo, VulnerabilityID, VulnerabilityInfo
 from typing import Dict, Iterator, cast, Tuple
+import nmapExtract
+import Nessusextract
+from cyberbattle.simulation import model as m
 
 default_allow_rules = [
     m.FirewallRule("RDP", m.RulePermission.ALLOW),
@@ -16,244 +23,40 @@ default_allow_rules = [
     m.FirewallRule("HTTPS", m.RulePermission.ALLOW),
     m.FirewallRule("HTTP", m.RulePermission.ALLOW)]
 
-# Network nodes involved in the Capture the flag game
-nodes = {
-    "Website": m.NodeInfo(
-        services=[m.ListeningService("HTTPS"),
-                  m.ListeningService("SSH", allowedCredentials=[
-                      "ReusedMySqlCred-web"])],
-        firewall=m.FirewallConfiguration(incoming=default_allow_rules,
-                                         outgoing=default_allow_rules + [
-                                             m.FirewallRule("su", m.RulePermission.ALLOW),
-                                             m.FirewallRule("sudo", m.RulePermission.ALLOW)]),
-        value=100,
-        # If can SSH into server then gets FLAG "Shared credentials with
-        # database user"
-        properties=["MySql", "Ubuntu", "nginx/1.10.3"],
-        owned_string="FLAG: Login using insecure SSH user/password",
-        vulnerabilities=dict(
-            ScanPageContent=m.VulnerabilityInfo(
-                description="LeakedGitHubProjectUrl: Website page content shows a link to GitHub "
-                            "repo",
-                type=m.VulnerabilityType.REMOTE,
-                outcome=m.LeakedNodesId(["GitHubProject"]),
-                reward_string="WEBSITE page content has a link to github -> Github project discovered!",
-                cost=1.0
-            ),
-            ScanPageSource=m.VulnerabilityInfo(
-                description="Website page source contains refrence to browseable "
-                            "relative web directory",
-                type=m.VulnerabilityType.REMOTE,
-                outcome=m.LeakedNodesId(["Website.Directory"]),
-                reward_string="Viewing the web page source reveals a URL to a .txt file and directory on the website",
-                cost=1.0
-            ),
-            CredScanBashHistory=m.VulnerabilityInfo(
-                description="bash history leaking creds - FLAG Stealing "
-                            "credentials for the monitoring user",
-                type=m.VulnerabilityType.LOCAL,
-                outcome=m.LeakedCredentials(credentials=[
-                    m.CachedCredential(node="Website[user=monitor]", port="SSH",
-                                       credential="monitorBashCreds")]),
-                reward_string="FLAG: SSH history revealed credentials for the monitoring user (monitor)",
-                cost=1.0
-            ))),
 
-    "Website.Directory": m.NodeInfo(
-        services=[m.ListeningService("HTTPS")],
-        value=50,
-        properties=["Ubuntu", "nginx/1.10.3",
-                    "CTFFLAG:Readme.txt-Discover secret data"
-                    ],
-        vulnerabilities=dict(
-            NavigateWebDirectoryFurther=m.VulnerabilityInfo(
-                description="Discover MYSQL credentials MySql for user "
-                            "'web' in (getting-started.txt)",
-                type=m.VulnerabilityType.REMOTE,
-                outcome=m.LeakedCredentials(credentials=[
-                    m.CachedCredential(node="Website", port="MySQL",
-                                       credential="ReusedMySqlCred-web")]),
-                reward_string="Discover browseable web directory: Navigating to parent URL revealed file `readme.txt`"
-                              "with secret data (aflag); and `getting-started.txt` with MYSQL credentials",
-                cost=1.0
-            ),
-            NavigateWebDirectory=m.VulnerabilityInfo(
-                description="Discover URL to external sharepoint website "
-                            "(in deprecation-checklist.txt)",
-                type=m.VulnerabilityType.REMOTE,
-                outcome=m.LeakedNodesId(["Sharepoint"]),
-                reward_string="Navigating to parent URL revealed file `deprecation-checklist.txt` containing"
-                              "a URL to an external sharepoint website",
-                cost=1.0
-            )
-        )),
+# +
 
-    "Website[user=monitor]": m.NodeInfo(
-        services=[m.ListeningService("SSH", allowedCredentials=[]),
-                  m.ListeningService("SSH-key", allowedCredentials=["unkownkey"]),
-                  m.ListeningService("su", allowedCredentials=["monitorBashCreds"])],
-        value=100,
-        properties=["MySql", "Ubuntu", "nginx/1.10.3"],
-        owned_string="FLAG User escalation by stealing credentials from bash history",
-        firewall=m.FirewallConfiguration(
-            outgoing=default_allow_rules,
-            incoming=[m.FirewallRule("SSH", m.RulePermission.BLOCK,
-                                     reason="password authentication disabled! SSH needs private key to authenticate."),
-                      m.FirewallRule("sudo", m.RulePermission.BLOCK,
-                                     reason="`sudo -u monitor` failed. User 'monitor' not sudoable."
-                                            "This warning will be reported!"),
-                      m.FirewallRule("su", m.RulePermission.ALLOW)] + default_allow_rules
-        ),
-        vulnerabilities={
-            "CredScan-HomeDirectory":
-                m.VulnerabilityInfo(
-                    description="azurecredential.txt file in home directory",
-                    type=m.VulnerabilityType.LOCAL,
-                    outcome=m.LeakedCredentials(credentials=[
-                        m.CachedCredential(
-                                node="AzureResourceManager[user=monitor]",
-                                port="HTTPS",
-                                credential="azuread_user_credentials")]),
-                    reward_string="SSH: cat ~/azurecreds.txt (running as monitor) revealed Azure user credential!",
-                    cost=1.0),
-        }),
+def cve_check(data):
+    total = []
+    for x in data:
+        cve = []
+        temp = []
+        cve.append(x[0])
+        for info in x[1]:
+            try:
+                temp.append(info['cve']) 
+            except KeyError:
+                   continue
+        cve.append(temp)
+        total.append(cve) #add node name and cve number
+    return total
 
-    "GitHubProject": m.NodeInfo(
-        services=[m.ListeningService("GIT")],
-        value=10,
-        properties=["GitHub", "SasUrlInCommit"],
-        vulnerabilities=dict(
-            CredScanGitHistory=m.VulnerabilityInfo(
-                description="Some secure access token (SAS) leaked in a "
-                "reverted git commit",
-                type=m.VulnerabilityType.REMOTE,
-                precondition=m.Precondition('SasUrlInCommit&GitHub'),
-                outcome=m.LeakedCredentials(credentials=[
-                    m.CachedCredential(node="AzureStorage",
-                                       port="HTTPS",
-                                       credential="SASTOKEN1")]),
-                rates=m.Rates(probingDetectionRate=0.0,
-                              exploitDetectionRate=0.0,
-                              successRate=1.0),
-                reward_string="CredScan success: Some secure access token (SAS) was leaked in a reverted git commit",
-                cost=1.0
-            ))),
-
-    "AzureStorage": m.NodeInfo(
-        services=[
-            m.ListeningService("HTTPS", allowedCredentials=["SASTOKEN1"])],
-        value=50,
-        properties=["CTFFLAG:LeakedCustomerData"],
-        vulnerabilities=dict(
-            AccessDataWithSASToken=m.VulnerabilityInfo(
-                description="Stealing secrets using a publicly shared "
-                            "SAS token",
-                type=m.VulnerabilityType.REMOTE,
-                outcome=m.CustomerData(),
-                rates=m.Rates(successRate=1.0),
-                reward_string="Stole data using a publicly shared SAS token",
-                cost=1.0
-            )
-        )),
-
-    'Sharepoint': m.NodeInfo(
-        services=[m.ListeningService("HTTPS")],
-        value=100,
-        properties=["SharepointLeakingPassword"],
-        firewall=m.FirewallConfiguration(incoming=[m.FirewallRule("SSH", m.RulePermission.ALLOW),
-                                                   m.FirewallRule("HTTP", m.RulePermission.ALLOW),
-                                                   m.FirewallRule("HTTPS", m.RulePermission.ALLOW)],
-                                         outgoing=[]),
-        vulnerabilities=dict(
-            ScanSharepointParentDirectory=m.VulnerabilityInfo(
-                description="Navigate to SharePoint site, browse parent "
-                            "directory",
-                type=m.VulnerabilityType.REMOTE,
-                outcome=m.LeakedCredentials(credentials=[
-                    m.CachedCredential(node="AzureResourceManager",
-                                       port="HTTPS",
-                                       credential="ADPrincipalCreds")]),
-                rates=m.Rates(successRate=1.0),
-                reward_string="Navigating to the Sharepoint site revealed  AD Service Principal Credentials",
-                cost=1.0)
-        )),
-
-    'AzureResourceManager': m.NodeInfo(
-        services=[m.ListeningService("HTTPS", allowedCredentials=["ADPrincipalCreds", "azuread_user_credentials"])],
-        owned_string="FLAG: Shared credentials with database user - Obtained secrets hidden in Azure Managed Resources",
-        value=50,
-        properties=["CTFFLAG:LeakedCustomerData2"],
-        vulnerabilities=dict(
-            ListAzureResources=m.VulnerabilityInfo(
-                description="AzureVM info, including public IP address",
-                type=m.VulnerabilityType.REMOTE,
-                outcome=m.LeakedNodesId(["AzureVM"]),
-                reward_string="Obtained Azure VM and public IP information",
-                cost=1.0
-            ))),
-
-    'AzureResourceManager[user=monitor]': m.NodeInfo(
-        services=[m.ListeningService("HTTPS", allowedCredentials=["azuread_user_credentials"])],
-        owned_string="More secrets stolen when logged as interactive `monitor` user in Azure with `az`",
-        value=50,
-        properties=[],
-    ),
-
-    'AzureVM': m.NodeInfo(
-        services=[m.ListeningService("PING"),
-                  m.ListeningService("SSH")],
-        value=100,
-        properties=["CTFFLAG:VMPRIVATEINFO"],
-        firewall=m.FirewallConfiguration(
-            incoming=[m.FirewallRule("SSH", m.RulePermission.BLOCK,
-                                     reason="internet incoming traffic blocked on the VM by NSG firewall")],
-            outgoing=[])),
-
-    'client': m.NodeInfo(
-        services=[],
-        value=0,
-        vulnerabilities=dict(
-            SearchEdgeHistory=m.VulnerabilityInfo(
-                description="Search web history for list of accessed websites",
-                type=m.VulnerabilityType.LOCAL,
-                outcome=m.LeakedNodesId(["Website"]),
-                reward_string="Web browser history revealed website URL of interest",
-                cost=1.0
-            )),
-        agent_installed=True,
-        reimagable=False),
-}
-
-'''global_vulnerability_library: Dict[VulnerabilityID, VulnerabilityInfo] = dict([])
-
-# Environment constants
-ENV_IDENTIFIERS = m.infer_constants_from_nodes(
-    cast(Iterator[Tuple[NodeID, NodeInfo]], list(nodes.items())),
-    global_vulnerability_library)
-
-
-def new_environment() -> m.Environment:
-    return m.Environment(
-        network=m.create_network(nodes),
-        vulnerability_library=global_vulnerability_library,
-        identifiers=ENV_IDENTIFIERS
-    )
-'''
-
-
-#newly added working codes
-import nmapextract
-import jsons
-import Nessusextract
-from cyberbattle.simulation import model as m
-
-nessusoutput = Nessusextract.processNessus("VA_Network_Scan_7hubkd (1).nessus")
-def vul_handler(name,data):
+def vul_handler(name,data): #To add vulnerabilities from nessus
     required= []
     for x in data:
         if x[0] == name:
             required = x[1]
-    for info in required:
+    for info in required: 
+        access_level = int(info['severity'])
+        if access_level == 0:
+            vul_outcome = m.CustomerData()
+        elif access_level == 1:
+            vul_outcome = m.PrivilegeEscalation(1)
+        elif access_level == 2:
+            vul_outcome = m.AdminEscalation()
+        elif access_level == 3:
+            vul_outcome = m.SystemEscalation()
+            
         vultype = 'm.VulnerabilityType.{}, '.format(info['plugin_type'].upper())
         vulnames = info['pluginName'].replace(" ", "")
         vulnerabilities = { 
@@ -261,101 +64,72 @@ def vul_handler(name,data):
             vulnames : m.VulnerabilityInfo(
             description = info['description'],
             type=eval(vultype)[0], #[0] was used as eval convert vultype into a tuple 
-            outcome=m.CustomerData(),
+            outcome=vul_outcome, #fake as customer data
             ),
-                'SearchEdgeHistory':m.VulnerabilityInfo(
-                description="Search web history for list of accessed websites",
-                type=m.VulnerabilityType.LOCAL,
-                outcome=m.LeakedNodesId(['192.168.242.132']),
-                reward_string="Web browser history revealed website URL of interest",
-                cost=1.0
-            ),
-            "CredScan-HomeDirectory":
-                m.VulnerabilityInfo(
-                    description="azurecredential.txt file in home directory",
-                    type=m.VulnerabilityType.LOCAL,
-                    outcome=m.LeakedCredentials(credentials=[
-                        m.CachedCredential(
-                                node="192.168.242.132'",
-                                port="HTTPS",
-                                credential="azuread_user_credentials")]),
-                    reward_string="SSH: cat ~/azurecreds.txt (running as monitor) revealed Azure user credential!",
-                    cost=1.0),
             }
+        
         return vulnerabilities
 
-def addnode(filename):
-    data = xmlextract.processdata(filename)
-    test = {}
+
+
+# +
+
+
+def addnode(nmapfilename, nessusfilename):
+    data = nmapExtract.processdata(nmapfilename) # get nmap data process
+    nessusoutput = Nessusextract.processNessus(nessusfilename)
+    nodeinfo = {}
     node_ids = []
     for x in data:
-        x[1] = list(dict.fromkeys(x[1]))
+        x[1] = list(dict.fromkeys(x[1])) 
         rules = ''
-        print(x[1])
         for service in x[1]:
             rules += service
-        propertiesa= x[2]
-
-        print('---------------')
-        print(rules)
-        print('---------------')
+        propertiesa= x[2] #properties from the nmap about the machine
         random = compile(rules,'string', 'eval')
-        print(eval(random))
-        print('before this is exec')
-        test[str(x[0])] = m.NodeInfo(
-            services= list(eval(random)),
-            firewall=m.FirewallConfiguration(incoming=default_allow_rules,
+        nodeinfo[str(x[0])] = m.NodeInfo( #node name added
+            services= list(eval(random)), # services added
+            firewall=m.FirewallConfiguration(incoming=default_allow_rules, # default firewall rules
                                              outgoing=default_allow_rules + [
                                                  m.FirewallRule("su", m.RulePermission.ALLOW),
                                                  m.FirewallRule("sudo", m.RulePermission.ALLOW)]),
-            value=0,
-            # If can SSH into server then gets FLAG "Shared credentials with
-            # database user"
-            properties=[propertiesa],
-            vulnerabilities= vul_handler(str(x[0]),nessusoutput)
+            value=10,
+            properties=[propertiesa], 
+            vulnerabilities= vul_handler(str(x[0]),nessusoutput) #add vulnerbilities with the vul_handler function
         )
         node_ids.append(x[0])
         
-    test['client'] = m.NodeInfo(
+    nodeinfo['client'] = m.NodeInfo(
     services=[],
     properties=["CLIENT:Kali"],
     value=0,
     vulnerabilities=dict(
-        SearchEdgeHistory=m.VulnerabilityInfo(
-            description="Search web history for list of accessed websites",
+        nmapscan=m.VulnerabilityInfo( #default vulnerbilities running nmap 
+            description="Scan the network using Nmap",
             type=m.VulnerabilityType.LOCAL,
             outcome=m.LeakedNodesId(node_ids),
-            reward_string="Web browser history revealed website URL of interest",
+            reward_string="new nodes discovered",
+            cost=1.0
+        ),
+        mimikatz=m.VulnerabilityInfo(  #default vulnerbilities running mimikatz
+            description="mimikatz password account crack",
+            type=m.VulnerabilityType.REMOTE,
+            outcome=m.LeakedCredentials(credentials=[
+                    m.CachedCredential(node=node_ids[0],
+                                       port="HTTPS",
+                                       credential="normaluser")]),
+            reward_string="Credentials for normal access",
             cost=1.0
         )),
     agent_installed=True,
-    reimagable=False)
+    reimagable=False)                      
+    cve_info= cve_check(nessusoutput)                
+    return nodeinfo, cve_info
 
-                                         
-                                         
-    return test 
-    '''rules = ''
-        print(x[1])
-        for service in x[1]:
-            rules += service
-        propertiesa= x[2]
-        test = {
-            x[0]: m.NodeInfo(
-                services=[eval(rules)],
-                firewall=m.FirewallConfiguration(incoming=default_allow_rules,
-                                                 outgoing=default_allow_rules + [
-                                                     m.FirewallRule("su", m.RulePermission.ALLOW),
-                                                     m.FirewallRule("sudo", m.RulePermission.ALLOW)]),
-                value=100,
-                # If can SSH into server then gets FLAG "Shared credentials with
-                # database user"
-                properties=[propertiesa],
-                owned_string="FLAG: Login using insecure SSH user/password",
-                vulnerabilities=dict(
-                    )),
-        }'''
-node_a = addnode('nmapScan.xml')
-print(node_a)
+node_a, cve_info = addnode('nmapScan.xml',"VA_Network_Scan_7hubkd (1).nessus" ) #Change the Filename to the Nmap scan output
+
+
+
 # Environment constants
 global_vulnerability_library: Dict[VulnerabilityID, VulnerabilityInfo] = dict([])
 def new_environment() -> m.Environment:
@@ -367,5 +141,8 @@ def new_environment() -> m.Environment:
 ENV_IDENTIFIERS = m.infer_constants_from_nodes(
     cast(Iterator[Tuple[NodeID, NodeInfo]], list(node_a.items())),
     global_vulnerability_library)
+
+
+
 
 
